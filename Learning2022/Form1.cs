75873ab016d0f2189370b3b95138f4fee0e61f2a
princessofchocolate <mary.sean.v@gmail.com>
2022-10-18 21:24:06 +0300

    initial commit

---
+++ Learning2022/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Learning2022 {
+    public partial class Form1 : Form {
+
+        private Learning2022.Forms.BubbleSortForm bsfForm = new Forms.BubbleSortForm();
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnBubbleSort_Click(object sender, EventArgs e) {
+            bsfForm.ShowDialog();
+        }
+    }
+}
